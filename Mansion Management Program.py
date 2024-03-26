125c49b332d950253957441ec6ae3601e3af2be6
Jeyavarshan Js <164999266+JeyavarshanJs@users.noreply.github.com>
2024-03-26 16:17:15 +0530

    Update Mansion Management Program.py

--- Mansion Management Program.py
+++ Mansion Management Program.py
@@ -3,7 +3,6 @@ import math, datetime, calendar
 import os, sys
 from PIL import Image, ImageDraw, ImageFont
 
-# Testing  .................................................
 # UPDATE
 def Update_TenantName_Field(TableName):
     if TableName == SUB_MENU_UPDATE_TenantName[1]:
